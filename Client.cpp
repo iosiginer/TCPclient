e5adeabfe28f1297f99455b78e98d613e1815ab8
barak1224 <barak1224@gmail.com>
2017-12-07 10:10:59 +0200

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,8 +1,3 @@
-/**
- * Created by Josef Ginerman on 04/12/17.
- * ID : 332494830
- **/
-
 #include "Client.h"
 #include <iostream>
 #include <sys/socket.h>
@@ -11,15 +6,12 @@
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>
-
 using namespace std;
-
-Client::Client(const char *serverIP, int serverPort) :
+Client::Client(const char *serverIP, int serverPort):
         serverIP(serverIP), serverPort(serverPort),
         clientSocket(0) {
     cout << "Client" << endl;
 }
-
 void Client::connectToServer() {
     // Create a socket point
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -33,46 +25,44 @@ void Client::connectToServer() {
     }
     // Get a hostent structure for the given host address
     struct hostent *server;
-    server = gethostbyaddr((const void *) &address, sizeof
+    server = gethostbyaddr((const void *)&address, sizeof
             address, AF_INET);
     if (server == NULL) {
         throw "Host is unreachable";
     }
     // Create a structure for the server address
     struct sockaddr_in serverAddress;
-    bzero((char *) &address, sizeof(address));
+    bzero((char *)&address, sizeof(address));
     serverAddress.sin_family = AF_INET;
-    memcpy((char *) &serverAddress.sin_addr.s_addr, (char
-    *) server->h_addr, server->h_length);
-    // htons converts values between host and network byteorders
+    memcpy((char *)&serverAddress.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+    // htons converts values between host and network byte orders
     serverAddress.sin_port = htons(serverPort);
     // Establish a connection with the TCP server
-    if (connect(clientSocket, (struct sockaddr
-    *) &serverAddress, sizeof(serverAddress)) == -1) {
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
         throw "Error connecting to server";
     }
     cout << "Connected to server" << endl;
+    int n = read(clientSocket, &playerNumber, sizeof(playerNumber));
+    cout << playerNumber << endl;
 }
 
-int Client::sendExercise(int arg1, char op, int arg2) {
-    // Write the exercise arguments to the socket
-    int n = write(clientSocket, &arg1, sizeof(arg1));
-    if (n == -1) {
-        throw "Error writing arg1 to socket";
-    }
-    n = write(clientSocket, &op, sizeof(op));
-    if (n == -1) {
-        throw "Error writing op to socket";
-    }
-    n = write(clientSocket, &arg2, sizeof(arg2));
-    if (n == -1) {
-        throw "Error writing arg2 to socket";
-    }
+void Client::readFunction() {
+    char result[9];
     // Read the result from the server
-    int result;
-    n = read(clientSocket, &result, sizeof(result));
+    cout << "Waiting for the message from the other player..." << endl;
+    int n = read(clientSocket, result, sizeof(result));
     if (n == -1) {
         throw "Error reading result from socket";
     }
-    return result;
+    cout << "The message received:" << result << endl;
+}
+
+void Client::writeFunction() {
+    char result[9];
+    cout << "Please enter a message:" << endl;
+    cin >> result;
+    int n = write(clientSocket, result, sizeof(result));
+    if (n == -1) {
+        throw "Error writing arg1 to socket";
+    }
 }
